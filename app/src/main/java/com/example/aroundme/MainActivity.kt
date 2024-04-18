7c5bf664d5f53ddf86d048c5f7bc858444a00538
Tashrique Ahmed <ta13@williams.edu>
2024-04-18 04:05:52 -0400

    Google Maps with Marker integrated

--- app/src/main/java/com/example/aroundme/MainActivity.kt
+++ app/src/main/java/com/example/aroundme/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.aroundme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
-class MainActivity : AppCompatActivity() {
+
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+//    private var mGoogleMap: GoogleMap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        // Add a marker in Sydney and move the camera
+        val userCoordinates = LatLng(37.7749, -122.4194)
+        googleMap.addMarker(com.google.android.gms.maps.model.MarkerOptions().position(userCoordinates).title("Marker in San Francisco"))
+        googleMap.moveCamera(com.google.android.gms.maps.CameraUpdateFactory.newLatLng(userCoordinates))
     }
 }
