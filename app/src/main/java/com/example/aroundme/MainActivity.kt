0b1540ac2c07edad0c8a8c391078867ed676e83d
Tashrique Ahmed <ta13@williams.edu>
2024-04-22 19:41:16 -0400

    Menu added to view seperate Activity

--- app/src/main/java/com/example/aroundme/MainActivity.kt
+++ app/src/main/java/com/example/aroundme/MainActivity.kt
@@ -6,8 +6,7 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Button
+
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
@@ -19,6 +18,10 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 
@@ -26,10 +29,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var map: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-
     private lateinit var tvGpsLocation: TextView
     private val locationPermissionCode = 2
-
 
 
 
@@ -39,19 +40,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.activity_main)
         title = "Around Me"
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        setupMenu()
+        setupMap()
 
 
-//        Get the location
-        tvGpsLocation = findViewById(R.id.gpsLocationText)
-        val button = findViewById<Button>(R.id.getLocation)
-        button.setOnClickListener {
-            getLocation()
-        }
-
-//        Show the map
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as? SupportMapFragment
-        mapFragment?.getMapAsync(this)
     }
 
     @SuppressLint("MissingPermission")
@@ -65,37 +57,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         }
 
         googleMap.uiSettings.isZoomControlsEnabled = true
-
-
-//        // I want to move the camera to my current location
-//        val userCoordinates = LatLng(40.7749, -122.4194)
-//        googleMap.uiSettings.isMyLocationButtonEnabled = true
-//
-//        googleMap.setOnMyLocationButtonClickListener {
-//            onMyLocationButtonClick()
-//        }
-//
-//        googleMap.setOnMyLocationClickListener {
-//            onMyLocationClick(it)
-//        }
-//
-//
-//        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userCoordinates, 13f))
-
     }
 
-    private fun onMyLocationClick(location: Location) {
-        Toast.makeText(this, "Current location:\n$location", Toast.LENGTH_LONG)
-            .show()
-    }
-
-    private fun onMyLocationButtonClick(): Boolean {
-        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT)
-            .show()
-        // Return false so that we don't consume the event and the default behavior still occurs
-        // (the camera animates to the user's current position).
-        return false
-    }
 
 
     private fun getLocation() {
@@ -133,5 +96,40 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             map.isMyLocationEnabled = true
         }
+    }
+
+    private fun setupMenu() {
+        val mapButton = findViewById<Button>(R.id.mapButton)
+        val listButton = findViewById<Button>(R.id.listButton)
+        val settingsButton = findViewById<Button>(R.id.settingsButton)
+        mapButton.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
+
+        listButton.setOnClickListener {
+            val intent = Intent(this, ListActivity::class.java)
+            startActivity(intent)
+        }
+
+        settingsButton.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun setupMap() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+//        Get the location
+        tvGpsLocation = findViewById(R.id.gpsLocationText)
+        val button = findViewById<Button>(R.id.getLocation)
+        button.setOnClickListener {
+            getLocation()
+        }
+
+//        Show the map
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapFragment) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
     }
 }
